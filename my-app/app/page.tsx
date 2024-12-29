cfc2195166be2ba648d2dbfc53252218a88bf1b3
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-12-29 22:59:54 +0530

    Set up Hero section

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="dark">
+        <Hero />
+      </div>
+    </main>
+  );
+}
