c986d369393409e00044eab5c18f4783a3dc9f88
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-01-02 00:23:30 +0530

    fixes

--- my-app/components/Projects.tsx
+++ my-app/components/Projects.tsx
@@ -9,12 +9,16 @@ export const Projects = () => {
   return (
     <div className="py-20">
       {/* Section Title */}
-      <div className="flex flex-col items-center text-center mb-10">
-        <h1 className="text-3xl font-bold text-white">
-          <span className="text-cyan-500 flex items-center gap-2">
-            Projects <IconCode size={36} />
-          </span>
-        </h1>
+      <div className="flex flex-col items-center mb-10">
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-gray-200 to-gray-400 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-gray-600">
+          Projects
+        </span>
+        
+        {/* Icon with a simple gray color */}
+        <IconCode
+          size={56}
+          className="mt-2 text-gray-500 dark:text-gray-300"
+        />
       </div>
 
       <div className="max-w-full mx-auto px-1">
--- my-app/components/TechStack.tsx
+++ my-app/components/TechStack.tsx
@@ -3,11 +3,13 @@ import { IconCloud } from "./ui/icons-cloud";
 export const TechStack = () => {
     return (
       <div className="pt-5">
-        <div className="flex justify-center items-center">
-            <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-gray-200 to-gray-400 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-gray-600">
+        <div className="flex justify-center">
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-gray-200 to-gray-400 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-gray-600">
                 My Tech Stack
-            </span>
-          <div className="relative flex h-[700px] w-full max-w-lg items-center justify-center overflow-hidden rounded-lg bg-background px-8">
+        </span>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="relative flex h-[500px] w-full max-w-lg items-center justify-center overflow-hidden rounded-lg bg-background px-8">
             {/* IconCloud Component */}
             <IconCloud iconSlugs={slugs} />
           </div>
